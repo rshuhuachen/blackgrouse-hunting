2108d8e4ac9fdead601429c38fba4701b26b8af3
rshuhuachen <rschen96@gmail.com>
2022-05-19 17:57:18 +0200

    add density data again

--- 3b.STRUCTUREonechickbrood.R
+++ 3b.STRUCTUREonechickbrood.R
@@ -1,5 +1,6 @@
 ###### STRUCTURE b - taking only one chick per brood ######
 
+library(readxl); library(tidyverse)
 # first make a table for GenAlEx
 chicks <- read.csv("data/cleandata/Unsplit.microsat.chicks.noLOCUS1+13+14.csv")
 
@@ -7,7 +8,7 @@ chicks.stru <- read.table("data/cleandata/Microsat.chicks.noLOCUS1+13+14.forstru
 
 pops <- read.csv("data/details/Codes.pops.both.filtered_withcoord.csv")
 
-
+### combine with
 chicks.df <- left_join(chicks, pops[,c(2,3)], by = c("pop" = "pop_num"))
 chicks.hunted <- subset(chicks.df, hunt == "hunted")
 chicks.unhunted <- subset(chicks.df, hunt == "unhunted")
