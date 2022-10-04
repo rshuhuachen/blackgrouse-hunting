8c97d4724f5581314e67490128db22d31260c21e
rshuhuachen <rebecca.chen@uni-bielefeld.de>
2022-10-04 13:53:24 +0200

    grendizer migration

--- analyses/R/5.migration.R
+++ analyses/R/5.migration.R
@@ -82,6 +82,7 @@ system(paste0("mkdir ", getwd(), "/analyses/migrationanalysis/BA3runs/run5"))
 
 #5 runs with 5 different random seeds
 pathba3 <- "/Users/vistor/Documents/Work/Bielefeld/PhD/Software/BA3-migration/"
+pathba3 <- "~/"
 system(paste0(pathba3, "BA3/BA3MSAT -v -t -g -u -a 0.30 -f 0.40 -s 65323 -i 10000000 -b 1000000 -n 1000 -o run1.txt ", getwd(), "/analyses/migrationanalysis/data_females_ba3.txt")) 
 system(paste0(pathba3, "BA3/BA3MSAT -v -t -g -u -a 0.30 -f 0.40 -s 76553 -i 10000000 -b 1000000 -n 1000 -o run2.txt ", getwd(), "/analyses/migrationanalysis/data_all_ba3.txt")) 
 system(paste0(pathba3, "BA3/BA3MSAT -v -t -g -u -a 0.30 -f 0.40 -s 124643 -i 10000000 -b 1000000 -n 1000 -o run3.txt ", getwd(), "/analyses/migrationanalysis/data_all_ba3.txt")) 
